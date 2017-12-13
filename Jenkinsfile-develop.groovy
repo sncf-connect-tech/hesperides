78e0fe97fb0b0d61ea72b28658093cc8f83afc1f
thomas_lhostis <tlhostis@voyages-sncf.com>
2017-12-13 14:20:46 +0100

    Jenkinsfile :  étape "BUILD AND PUSH".

--- Jenkinsfile-develop.groovy
+++ Jenkinsfile-develop.groovy
@@ -1,55 +1,57 @@
 pipeline {
 	agent any
+	tools {
+		maven 'maven3'
+	}
 	stages {
 		stage('BUILD AND PUSH') {
 			steps {
-				// Maven clean deploy + TU
-				sleep 1
+				sh 'mvn clean deploy'
 			}
 		}
 		stage('DEPLOY DEV') {
 			steps {
 				// Appeler un job rundeck (pprundeck.start...)
-				sleep 1
+				sleep 0
 			}
 		}
 		stage('MOCKED TESTS') {
 			steps {
 				// Tests bouchonnés (BDD, profil Maven)
-				sleep 1
+				sleep 0
 			}
 		}
 		stage('DEPLOY INT') {
 			steps {
 				// Appeler un job rundeck (pprundeck.start...)
-				sleep 1
+				sleep 0
 			}
 		}
 		stage('INTEGRATION TESTS') {
 			steps {
 				// Tests débouchonnés (BDD, profil Maven)
-				sleep 1
+				sleep 0
 			}
 		}
 		stage('DEPLOY PERF') {
 			steps {
 				// Créer une plateforme de tests de perf
 				// Appeler un job rundeck (pprundeck.start...)
-				sleep 1
+				sleep 0
 			}
 		}
 		stage('PERF TESTS') {
 			steps {
 				// Tests de perf (tir gatling:execute)
-				sleep 1
+				sleep 0
 			}
 		}
 		stage('RELEASE') {
 			steps {
-				input 'Release?'
-				milestone 1
+				//input 'Release?'
+				//milestone 1
 				// milestone + maven release (crée la branche release et déploie sur le nexus)
-				sleep 1
+				sleep 0
 			}
 		}
 	}
