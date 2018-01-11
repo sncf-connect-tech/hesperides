d0c912687ab5f983a7d4baf42720ef64fc7eb615
thomas_lhostis <thomas.lhostis@gmail.com>
2018-01-11 10:47:32 +0100

    C'est mieux si on l'ignore pas

--- hesperides-legacy/src/test/java/bdd/CucumberTest.java
+++ hesperides-legacy/src/test/java/bdd/CucumberTest.java
@@ -32,10 +32,8 @@ import cucumber.api.java8.En;
 import cucumber.api.junit.Cucumber;
 import io.dropwizard.testing.junit.ResourceTestRule;
 import org.junit.ClassRule;
-import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
-@Ignore
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features/test.feature")
 public class CucumberTest implements En {
