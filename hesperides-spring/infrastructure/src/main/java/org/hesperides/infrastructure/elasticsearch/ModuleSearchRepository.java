20f4bbcd9ab18b50b615b0c7050f4eb555961d07
Maillard Sylvain <smaillard@voyages-sncf.com>
2018-01-19 17:20:25 +0100

    Init maven structure.

---
+++ hesperides-spring/infrastructure/src/main/java/org/hesperides/infrastructure/elasticsearch/ModuleSearchRepository.java
@@ -0,0 +1,43 @@
+/*
+ *
+ * This file is part of the Hesperides distribution.
+ * (https://github.com/voyages-sncf-technologies/hesperides)
+ * Copyright (c) 2016 VSCT.
+ *
+ * Hesperides is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, version 3.
+ *
+ * Hesperides is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
+package org.hesperides.infrastructure.elasticsearch;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class ModuleSearchRepository implements org.hesperides.domain.ModuleSearchRepository {
+
+    @Autowired
+    ModuleRepository moduleRepository;
+
+    @Override
+    public List<org.hesperides.domain.Module> getModules() {
+        List<org.hesperides.domain.Module> modules = new ArrayList<>();
+        for (Module elasticsearchModule : moduleRepository.findAll()) {
+            modules.add(elasticsearchModule.toDomainModule());
+        }
+        return modules;
+    }
+}
