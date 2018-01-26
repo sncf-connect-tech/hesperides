848fe97e07744156be2ab6b5253c04fb415c98a8
Maillard Sylvain <smaillard@voyages-sncf.com>
2018-01-26 16:52:29 +0100

    Ajout validation sur la conf ldap

--- hesperides-spring/infrastructure/src/main/java/org/hesperides/infrastructure/security/LdapConfiguration.java
+++ hesperides-spring/infrastructure/src/main/java/org/hesperides/infrastructure/security/LdapConfiguration.java
@@ -21,11 +21,19 @@
 package org.hesperides.infrastructure.security;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.annotation.PostConstruct;
+import javax.validation.constraints.NotNull;
 
 @Component
 @ConfigurationProperties("ldap")
+@Validated
 public class LdapConfiguration {
+
+    @NotNull
     private String url;
     private String domain;
     private String userSearchBase;
