ba5479ca69e9593971fb4c3ea856c1c90742f735
Maillard Sylvain <smaillard@voyages-sncf.com>
2018-01-26 16:51:54 +0100

    Créer l'index hesperides dans els si celui-ci n'est pas présent.

--- hesperides-spring/infrastructure/src/main/java/org/hesperides/infrastructure/elasticsearch/ElasticsearchService.java
+++ hesperides-spring/infrastructure/src/main/java/org/hesperides/infrastructure/elasticsearch/ElasticsearchService.java
@@ -22,10 +22,13 @@ package org.hesperides.infrastructure.elasticsearch;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpEntity;
 import org.apache.http.entity.ContentType;
 import org.apache.http.nio.entity.NStringEntity;
+import org.apache.http.util.EntityUtils;
 import org.elasticsearch.client.Response;
+import org.elasticsearch.client.ResponseException;
 import org.elasticsearch.client.RestClient;
 import org.hesperides.infrastructure.elasticsearch.response.ResponseHits;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +37,7 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.util.Collections;
 
+@Slf4j
 @Service
 public class ElasticsearchService {
     @Autowired
@@ -50,10 +54,42 @@ public class ElasticsearchService {
             HttpEntity entity = new NStringEntity(requestBody, ContentType.APPLICATION_JSON);
             Response response = restClient.performRequest(method, endpoint, Collections.emptyMap(), entity);
             responseHits = new ObjectMapper().readValue(response.getEntity().getContent(), typeReference);
+        } catch (ResponseException e) {
+
+            // si on est sur du 404, alors tente de créer l'index.
+            if (hesperidesIndexIsNotPresent(e)) {
+                log.warn("Hesperides index was not found, create it");
+                // on crée l'index et on recommence la requete.
+                createHesperidesIndex();
+                return getResponseHits(method, url, requestBody, typeReference);
+            }
+            throw new RuntimeException(e);
         } catch (IOException e) {
-            e.printStackTrace(); //TODO Gérer l'exception
+            log.error(e.getMessage(), e);
             throw new RuntimeException(e);
         }
         return responseHits;
+    }
+
+    private boolean hesperidesIndexIsNotPresent(ResponseException e) {
+        // si on est sur du 404, alors tente de créer l'index.
+        return e.getResponse().getStatusLine().getStatusCode() == 404 && entityContainsIndexMissingException(e.getResponse().getEntity());
+    }
+
+    private boolean entityContainsIndexMissingException(HttpEntity entity) {
+        try {
+            String e = EntityUtils.toString(entity);
+            return e != null && e.contains("IndexMissingException[[hesperides] missing]");
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    private void createHesperidesIndex() {
+        try {
+            elasticsearchClient.getRestClient().performRequest("PUT", "/" + this.elasticsearchConfiguration.getIndex());
+        } catch (IOException e) {
+            throw new RuntimeException("could not create hesperides index: " + e.getMessage(), e);
+        }
     }
 }
