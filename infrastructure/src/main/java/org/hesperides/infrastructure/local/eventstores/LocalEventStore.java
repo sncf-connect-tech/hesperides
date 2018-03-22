1b15bfef6cdbf3ac123b0de376b38e95e4f104a9
Sylvain <sylvain.maillard6@gmail.com>
2018-03-22 23:50:23 +0100

    Fix Tests

--- infrastructure/src/main/java/org/hesperides/infrastructure/local/eventstores/LocalEventStore.java
+++ infrastructure/src/main/java/org/hesperides/infrastructure/local/eventstores/LocalEventStore.java
@@ -1,18 +1,10 @@
 package org.hesperides.infrastructure.local.eventstores;
 
-import com.thoughtworks.xstream.XStream;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.FileUtils;
 import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore;
-import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
 import org.axonframework.eventsourcing.eventstore.inmemory.InMemoryEventStorageEngine;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.PreDestroy;
-import java.io.File;
-import java.io.IOException;
-import java.nio.charset.Charset;
 
 /**
  * In memory event storage if local profile is used
@@ -22,28 +14,7 @@ import java.nio.charset.Charset;
 @Profile("local")
 public class LocalEventStore extends EmbeddedEventStore {
 
-    private final static XStream xStream = new XStream();
-    private final static File localEventStoreFile = new File(FileUtils.getTempDirectory(),"hesperide_local_event_store.xml");
-
-    public LocalEventStore() throws IOException {
-        super(loadFromDisk());
-    }
-
-    @PreDestroy
-    public void saveToDisk() throws IOException {
-        log.debug("Save event store to file {}", localEventStoreFile);
-        String xml = xStream.toXML(this.storageEngine());
-        FileUtils.writeStringToFile(localEventStoreFile, xml, Charset.defaultCharset());
-    }
-
-    private static EventStorageEngine loadFromDisk() throws IOException {
-        // charge les events depuis le disque, pour voir.
-        if (localEventStoreFile.exists()) {
-            log.debug("Load event store from file {}", localEventStoreFile);
-            xStream.setClassLoader(Thread.currentThread().getContextClassLoader());
-            return (InMemoryEventStorageEngine) xStream.fromXML(FileUtils.readFileToString(localEventStoreFile, Charset.defaultCharset()));
-        }
-        // si pas de fichier, on créer en mémoire.
-        return new InMemoryEventStorageEngine();
+    public LocalEventStore() {
+        super(new InMemoryEventStorageEngine());
     }
 }
