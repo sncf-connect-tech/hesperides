8cd80c002f0364fc2a05fd81abc31baa4c47a03a
victorsalaun <victor.salaun@gmail.com>
2018-04-10 13:35:09 +0200

    implements postgresql module queries

--- infrastructure/src/main/java/org/hesperides/infrastructure/postgresql/modules/queries/PostgresqlModuleQueriesRepository.java
+++ infrastructure/src/main/java/org/hesperides/infrastructure/postgresql/modules/queries/PostgresqlModuleQueriesRepository.java
@@ -1,15 +1,20 @@
 package org.hesperides.infrastructure.postgresql.modules.queries;
 
+import org.axonframework.queryhandling.QueryHandler;
 import org.hesperides.domain.modules.*;
+import org.hesperides.domain.modules.entities.Module;
 import org.hesperides.domain.modules.queries.ModuleQueriesRepository;
 import org.hesperides.domain.modules.queries.ModuleView;
+import org.hesperides.infrastructure.postgresql.ModuleEntity;
 import org.hesperides.infrastructure.postgresql.modules.PostgresqlModuleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Profile("postgresql")
 @Component
@@ -22,29 +27,86 @@ public class PostgresqlModuleQueriesRepository implements ModuleQueriesRepositor
         this.postgresqlModuleRepository = postgresqlModuleRepository;
     }
 
+    @QueryHandler
     @Override
     public Optional<ModuleView> query(ModuleByIdQuery query) {
-        return Optional.empty();
+        ModuleEntity.ModuleEntityId id = new ModuleEntity.ModuleEntityId(
+                query.getModuleKey().getName(),
+                query.getModuleKey().getVersion(),
+                query.getModuleKey().getVersionType()
+        );
+        ModuleEntity moduleEntity = new ModuleEntity();
+        moduleEntity.setModuleEntityId(id);
+        moduleEntity = postgresqlModuleRepository.findOne(id);
+        if (moduleEntity == null) {
+            return Optional.empty();
+        }
+        return Optional.of(
+                new ModuleView(
+                        moduleEntity.getModuleEntityId().getName(),
+                        moduleEntity.getModuleEntityId().getVersion(),
+                        moduleEntity.getModuleEntityId().getVersionType() == Module.Type.workingcopy,
+                        moduleEntity.getVersionId()
+                )
+        );
     }
 
+    @QueryHandler
     @Override
     public List<String> queryAllModuleNames(ModulesNamesQuery query) {
-        return null;
+        return postgresqlModuleRepository.findAll()
+                .stream()
+                .map(ModuleEntity::getModuleEntityId)
+                .map(ModuleEntity.ModuleEntityId::getName)
+                .collect(Collectors.toList());
     }
 
+    @QueryHandler
     @Override
     public List<String> queryModuleTypes(ModuleTypesQuery query) {
-        return null;
+        ModuleEntity.ModuleEntityId id = new ModuleEntity.ModuleEntityId(
+                query.getModuleName(),
+                query.getModuleVersion(),
+                null
+        );
+        ModuleEntity moduleEntity = new ModuleEntity();
+        moduleEntity.setModuleEntityId(id);
+        return postgresqlModuleRepository.findAll(Example.of(moduleEntity))
+                .stream()
+                .map(ModuleEntity::getModuleEntityId)
+                .map(ModuleEntity.ModuleEntityId::getVersionType)
+                .map(Module.Type::toString)
+                .collect(Collectors.toList());
     }
 
+    @QueryHandler
     @Override
     public List<String> queryModuleVersions(ModuleVersionsQuery query) {
-        return null;
+        ModuleEntity.ModuleEntityId id = new ModuleEntity.ModuleEntityId(
+                query.getModuleName(),
+                null,
+                null
+        );
+        ModuleEntity moduleEntity = new ModuleEntity();
+        moduleEntity.setModuleEntityId(id);
+        return postgresqlModuleRepository.findAll(Example.of(moduleEntity))
+                .stream()
+                .map(ModuleEntity::getModuleEntityId)
+                .map(ModuleEntity.ModuleEntityId::getVersion)
+                .collect(Collectors.toList());
     }
 
+    @QueryHandler
     @Override
     public Boolean query(ModuleAlreadyExistsQuery query) {
-        return null;
+        ModuleEntity.ModuleEntityId id = new ModuleEntity.ModuleEntityId(
+                query.getModuleKey().getName(),
+                query.getModuleKey().getVersion(),
+                query.getModuleKey().getVersionType()
+        );
+        ModuleEntity moduleEntity = new ModuleEntity();
+        moduleEntity.setModuleEntityId(id);
+        return postgresqlModuleRepository.exists(Example.of(moduleEntity));
     }
 
 }
