e2b9abd6c798c8dbf7b930b26aee6332c70808e9
victorsalaun <victor.salaun@gmail.com>
2018-04-11 17:29:14 +0200

    remove redis

--- infrastructure/src/main/java/org/hesperides/infrastructure/mongo/MongoConfiguration.java
+++ infrastructure/src/main/java/org/hesperides/infrastructure/mongo/MongoConfiguration.java
@@ -1,15 +1,17 @@
 package org.hesperides.infrastructure.mongo;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
-@EnableAutoConfiguration
 @EnableMongoRepositories(basePackages = "org.hesperides.infrastructure.mongo")
+@Import({MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @Profile("mongo")
 public class MongoConfiguration {
 }
