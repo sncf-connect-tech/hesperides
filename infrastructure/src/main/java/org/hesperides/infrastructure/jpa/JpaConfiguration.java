6d69ca216ab314866e2bf19354b4d64e6e317e90
victorsalaun <victor.salaun@gmail.com>
2018-04-19 17:29:41 +0200

    add liquibase

--- infrastructure/src/main/java/org/hesperides/infrastructure/jpa/JpaConfiguration.java
+++ infrastructure/src/main/java/org/hesperides/infrastructure/jpa/JpaConfiguration.java
@@ -1,13 +1,18 @@
 package org.hesperides.infrastructure.jpa;
 
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableTransactionManagement
@@ -15,4 +20,16 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Profile("jpa")
 @Import({DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class JpaConfiguration {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public SpringLiquibase liquibase() {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:db/changelog/db.changelog-master.yaml");
+        liquibase.setDataSource(dataSource);
+        return liquibase;
+    }
+
 }
