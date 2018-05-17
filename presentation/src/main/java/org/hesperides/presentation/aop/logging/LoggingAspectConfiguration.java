6e038ab46c50be3bd95261a67d43ec1a9107a75b
victorsalaun <victor.salaun@gmail.com>
2018-05-17 22:53:40 +0200

    add aspect to log enter / exit on controllers

---
+++ presentation/src/main/java/org/hesperides/presentation/aop/logging/LoggingAspectConfiguration.java
@@ -0,0 +1,16 @@
+package org.hesperides.presentation.aop.logging;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class LoggingAspectConfiguration {
+
+    @Bean
+    public LoggingAspect loggingAspect() {
+        return new LoggingAspect();
+    }
+
+}
