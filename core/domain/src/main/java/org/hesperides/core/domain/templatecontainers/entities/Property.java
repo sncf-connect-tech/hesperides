68a020f8ade307c707ec7590342d3cbc0dff197d
Thomas L'Hostis <thomas.lhostis@gmail.com>
2018-07-24 14:59:54 +0200

    Gestion iso des droits null

--- core/domain/src/main/java/org/hesperides/core/domain/templatecontainers/entities/Property.java
+++ core/domain/src/main/java/org/hesperides/core/domain/templatecontainers/entities/Property.java
@@ -5,7 +5,6 @@ import lombok.Value;
 import org.apache.commons.lang3.StringUtils;
 import org.hesperides.core.domain.templatecontainers.exceptions.RequiredPropertyCannotHaveDefaultValueException;
 
-import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -27,7 +26,6 @@ public class Property extends AbstractProperty {
         this.pattern = pattern;
         this.isPassword = isPassword;
     }
-
 
     public void validate() {
         if (isRequired && !StringUtils.isEmpty(defaultValue)) {
