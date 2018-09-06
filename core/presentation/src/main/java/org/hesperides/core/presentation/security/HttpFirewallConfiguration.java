5ccda12c35f0afb58f7209b9fa5325b429687f02
Thomas L'Hostis <thomas.lhostis@gmail.com>
2018-09-06 15:48:57 +0200

    Fix #329

---
+++ core/presentation/src/main/java/org/hesperides/core/presentation/security/HttpFirewallConfiguration.java
@@ -0,0 +1,39 @@
+/*
+ *
+ * This file is part of the Hesperides distribution.
+ * (https://github.com/voyages-sncf-technologies/hesperides)
+ * Copyright (c) 2016 VSCT.
+ *
+ * Hesperides is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, version 3.
+ *
+ * Hesperides is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
+package org.hesperides.core.presentation.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
+
+@Configuration
+public class HttpFirewallConfiguration {
+
+    @Bean
+    public HttpFirewall httpFirewall() {
+        final StrictHttpFirewall httpFirewall = new StrictHttpFirewall();
+        // C'est une faille de sécurité mais le legacy le permet
+        // TODO Créer un test fonctionnel
+        httpFirewall.setAllowUrlEncodedPercent(true);
+        return httpFirewall;
+    }
+}
