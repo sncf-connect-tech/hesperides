2ac844461b664d64807e78afeea56fb758ffdb52
René Benjamin <benjamin.rene.90@gmail.com>
2018-09-14 10:26:51 +0200

    Add tests on event queries

---
+++ tests/bdd/src/test/java/org/hesperides/tests/bdd/events/EventsAssertions.java
@@ -0,0 +1,20 @@
+package org.hesperides.tests.bdd.events;
+
+
+import org.hamcrest.Matchers;
+import org.hesperides.core.presentation.io.events.EventOutput;
+import org.junit.Assert;
+
+import java.util.List;
+import java.util.Map;
+
+public class EventsAssertions {
+
+    public static void assertEventsSize(final List<EventOutput> events, final Integer size) {
+        Assert.assertThat(events, Matchers.hasSize(size));
+    }
+    public static void assertEventType(final EventOutput event, final String eventType) {
+        Assert.assertThat(event, Matchers.hasProperty("type", Matchers.endsWith(eventType)));
+    }
+
+}
