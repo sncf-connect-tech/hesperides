a4572e1c8796aad1babe87541b54ae342be5ac3a
Lucas Cimon <lucas.cimon@gmail.com>
2018-11-15 16:51:10 +0100

    Code cleanup to remove deprecated usages

--- core/presentation/src/main/java/org/hesperides/core/presentation/swagger/SwaggerConfiguration.java
+++ core/presentation/src/main/java/org/hesperides/core/presentation/swagger/SwaggerConfiguration.java
@@ -3,7 +3,7 @@ package org.hesperides.core.presentation.swagger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -13,7 +13,7 @@ import static springfox.documentation.builders.PathSelectors.any;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfiguration extends WebMvcConfigurerAdapter {
+public class SwaggerConfiguration implements WebMvcConfigurer {
 
     @Bean
     public Docket productApi() {
