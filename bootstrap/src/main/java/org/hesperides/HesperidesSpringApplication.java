3aeba9923d41fd36643062b0be770cefe71386c7
Thomas L'Hostis <thomas.lhostis@gmail.com>
2018-12-12 18:05:40 +0100

    Reset des exclusions des autoconfigurations

--- bootstrap/src/main/java/org/hesperides/HesperidesSpringApplication.java
+++ bootstrap/src/main/java/org/hesperides/HesperidesSpringApplication.java
@@ -2,8 +2,15 @@ package org.hesperides;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {
+                MongoAutoConfiguration.class,
+                MongoDataAutoConfiguration.class
+        }
+)
 public class HesperidesSpringApplication {
 
     public static void main(String[] args) {
