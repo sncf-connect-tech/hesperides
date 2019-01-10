6adb3d2b834063637aa2325baf170a307a6882b9
Lucas Cimon <lucas.cimon@gmail.com>
2019-01-10 18:28:12 +0100

    Making ensureCaseInsensitivity errors more explicit + adding a couple of comments

--- core/infrastructure/src/main/java/org/hesperides/core/infrastructure/mongo/MongoSearchOptions.java
+++ core/infrastructure/src/main/java/org/hesperides/core/infrastructure/mongo/MongoSearchOptions.java
@@ -33,11 +33,23 @@ public class MongoSearchOptions {
         MongoCollection<Document> collection = mongoTemplate.getCollection(collectionName);
         List<Document> indexes = StreamSupport.stream(collection.listIndexes().spliterator(), false).collect(Collectors.toList());
         Map<String, Document> indexPerName = indexes.stream().collect(Collectors.toMap(i -> (String)i.get("name"), Function.identity()));
-        if (2 != (Integer) ((Document) indexPerName.get("_id_").get("collation")).get("strength")) {
-            throw new RuntimeException("Collection ID is not case-insensitive");
+        if (2 != getCollationStrength(indexPerName, "_id_", collectionName)) {
+            throw new RuntimeException("Index \"_id\" in collection " + collectionName + " is not case-insensitive");
         }
-        if (2 != (Integer) ((Document) indexPerName.get("key_1").get("collation")).get("strength")) {
-            throw new RuntimeException("Collection key is not case-insensitive");
+        if (2 != getCollationStrength(indexPerName,"key_1", collectionName)) {
+            throw new RuntimeException("Index \"key\" in collection " + collectionName + " is not case-insensitive");
         }
+    }
+
+    private static Integer getCollationStrength(Map<String, Document> indexPerName, String indexName, String collectionName) {
+        Document index = indexPerName.get(indexName);
+        if (index == null) {
+            throw new RuntimeException("No index named " + indexName + " exists for collection " + collectionName);
+        }
+        Document collation = (Document) index.get("collation");
+        if (collation == null) {
+            throw new RuntimeException("No collation found for index " + indexName + " in collection " + collectionName);
+        }
+        return (Integer) collation.get("strength");
     }
 }
