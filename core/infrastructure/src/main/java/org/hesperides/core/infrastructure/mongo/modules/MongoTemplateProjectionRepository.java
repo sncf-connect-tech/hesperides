34164a454884351c55f8a38d0b5606442e44f920
Lucas Cimon <lucas.cimon@gmail.com>
2019-02-07 14:58:31 +0100

    data-migration: avoiding templates duplication

--- core/infrastructure/src/main/java/org/hesperides/core/infrastructure/mongo/modules/MongoTemplateProjectionRepository.java
+++ core/infrastructure/src/main/java/org/hesperides/core/infrastructure/mongo/modules/MongoTemplateProjectionRepository.java
@@ -64,7 +64,13 @@ public class MongoTemplateProjectionRepository implements TemplateProjectionRepo
         }
         ModuleDocument moduleDocument = optModuleDocument.get();
         TemplateDocument templateDocument = new TemplateDocument(event.getTemplate());
-        moduleDocument.addTemplate(templateDocument);
+        boolean templateAlreadyExist = false;  // this is only needed for the time of the migration, as checks are already done in the application layer
+        if (HasProfile.dataMigration()) {
+            templateAlreadyExist = moduleDocument.getTemplates().stream().anyMatch(existingTemplate -> existingTemplate.getName().equals(templateDocument.getName()));
+        }
+        if (!templateAlreadyExist) {
+            moduleDocument.addTemplate(templateDocument);
+        }
         if (HasProfile.dataMigration()) {
             moduleDocument.extractPropertiesAndSave(moduleRepository, Arrays.asList(event.getTemplate().getName()));
         } else {
