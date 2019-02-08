7f80bd5092d29407bbc1d58b891c599cbcef746e
Lucas Cimon <lucas.cimon@gmail.com>
2019-02-08 17:04:03 +0100

    Allowing slashes in module names & versions

--- bootstrap/src/main/java/org/hesperides/HesperidesSpringApplication.java
+++ bootstrap/src/main/java/org/hesperides/HesperidesSpringApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HesperidesSpringApplication {
 
     public static void main(String[] args) {
+        System.setProperty("org.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH", "true");
         SpringApplication.run(HesperidesSpringApplication.class, args);
     }
 }
