22bce29a0fe4a3d132d3fe965a35135ba7d33e82
Lucas Cimon <lucas.cimon@gmail.com>
2019-02-18 18:24:52 +0100

    Bugfix in Docker entrypoint

--- mongo_create_collections.js
+++ mongo_create_collections.js
@@ -1,4 +1,5 @@
 // On crée les 3 collections avec une collation rendant leur "key" insensible à la casse
+rs.slaveOk();
 ['module', 'platform', 'techno'].forEach(c => {
     db.createCollection(c, {collation: {locale: 'fr', strength: 2}})
     db[c].createIndex({key: 1})
