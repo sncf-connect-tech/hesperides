7e1edb5f5e582eb4396b6e954072e01b2b865a68
Lucas Cimon <lucas.cimon@gmail.com>
2019-02-18 19:21:46 +0100

    Bugfix for Docker entrypoint

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 set -o pipefail -o errexit -o nounset
 
+create_ok=$(mongo $PROJECTION_REPOSITORY_MONGO_URI --quiet --eval "db.createCollection('test').ok")
+if [ $create_ok -ne 1 ]; then
+    echo -e 'Not enough permissions to create a collection\nCheck that the cluster master node comes first in $PROJECTION_REPOSITORY_MONGO_URI' >&2
+    exit 1
+fi
+mongo $PROJECTION_REPOSITORY_MONGO_URI --eval "db.test.drop()"
+
 mongo $PROJECTION_REPOSITORY_MONGO_URI mongo_create_collections.js
 
 # If args were passed to this script, execute them as a command, else do nothing:
--- mongo_create_collections.js
+++ mongo_create_collections.js
@@ -1,8 +1,7 @@
 // On crée les 3 collections avec une collation rendant leur "key" insensible à la casse
-rs.slaveOk();
 ['module', 'platform', 'techno'].forEach(c => {
-    db.createCollection(c, {collation: {locale: 'fr', strength: 2}})
-    db[c].createIndex({key: 1})
+    printjson(db.createCollection(c, {collation: {locale: 'fr', strength: 2}}))
+    printjson(db[c].createIndex({key: 1}))
     print(c, 'indexes:')
     printjson(db[c].getIndexes())
 })
