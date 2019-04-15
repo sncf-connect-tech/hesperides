1948380d75399908cf9b449cc230db3e804da6ab
Lucas Cimon <lucas.cimon@gmail.com>
2019-04-15 16:04:20 +0200

    Fix WebSecurityConfig.AUTH_WHITELIST to authorize /rest/versions without auth

--- core/presentation/src/main/java/org/hesperides/core/presentation/security/WebSecurityConfig.java
+++ core/presentation/src/main/java/org/hesperides/core/presentation/security/WebSecurityConfig.java
@@ -44,6 +44,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private AuthenticationProvider authenticationProvider;
 
     private static final String[] AUTH_WHITELIST = {
+            "/versions",
             "/manage/**",
             // -- swagger ui
             "/swagger-resources/**",
