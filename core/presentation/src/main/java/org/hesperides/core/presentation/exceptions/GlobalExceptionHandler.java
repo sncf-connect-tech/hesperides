0e2100f4d1b21370d85eb60771e9a22f9787482a
Thomas L'Hostis <thomas.lhostis@gmail.com>
2019-04-17 16:58:29 +0200

    Search

--- core/presentation/src/main/java/org/hesperides/core/presentation/exceptions/GlobalExceptionHandler.java
+++ core/presentation/src/main/java/org/hesperides/core/presentation/exceptions/GlobalExceptionHandler.java
@@ -16,6 +16,7 @@ import org.hesperides.core.domain.templatecontainers.exceptions.RequiredProperty
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -57,7 +58,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
-    @ExceptionHandler(ForbiddenOperationException.class)
+    @ExceptionHandler({ForbiddenOperationException.class, AccessDeniedException.class})
     public ResponseEntity handleUnauthorized(Exception ex) {
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ex.getMessage());
     }
