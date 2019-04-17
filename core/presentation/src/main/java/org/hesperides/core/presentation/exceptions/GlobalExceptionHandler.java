913d43457849bff7304eed85c242074a20be80c6
Thomas L'Hostis <thomas.lhostis@gmail.com>
2019-04-17 12:55:07 +0200

    Gestion des erreurs non inattendues (#604)

--- core/presentation/src/main/java/org/hesperides/core/presentation/exceptions/GlobalExceptionHandler.java
+++ core/presentation/src/main/java/org/hesperides/core/presentation/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,8 @@
 package org.hesperides.core.presentation.exceptions;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.axonframework.commandhandling.model.AggregateNotFoundException;
-import org.axonframework.queryhandling.QueryExecutionException;
 import org.hesperides.core.domain.exceptions.DuplicateException;
 import org.hesperides.core.domain.exceptions.ForbiddenOperationException;
 import org.hesperides.core.domain.exceptions.NotFoundException;
@@ -11,14 +12,15 @@ import org.hesperides.core.domain.platforms.exceptions.InexistantPlatformAtTimeE
 import org.hesperides.core.domain.platforms.exceptions.InvalidPropertyValorisationException;
 import org.hesperides.core.domain.technos.exception.UndeletableTechnoInUseException;
 import org.hesperides.core.domain.templatecontainers.exceptions.RequiredPropertyWithDefaultValueException;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import org.springframework.web.util.WebUtils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Centralisation de la gestion des exceptions (bien pratique)
@@ -51,7 +53,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
-    @ExceptionHandler({ForbiddenOperationException.class})
+    @ExceptionHandler(ForbiddenOperationException.class)
     public ResponseEntity handleUnauthorized(Exception ex) {
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ex.getMessage());
     }
@@ -67,11 +69,21 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
-    @Override
-    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        if (HttpStatus.INTERNAL_SERVER_ERROR.equals(status)) {
-            request.setAttribute(WebUtils.ERROR_EXCEPTION_ATTRIBUTE, ex, WebRequest.SCOPE_REQUEST);
-        }
-        return new ResponseEntity<>(body != null ? body : ex.getMessage(), headers, status);
+    /**
+     * Exceptions non gérées
+     *
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity handleUnexpectedException(Exception ex, WebRequest request) {
+        logger.error(ex);
+        Map<String, Object> jsonData = new HashMap<>();
+        jsonData.put("message", StringUtils.isNotEmpty(ex.getMessage()) ? ex.getMessage() : ex.toString());
+        jsonData.put("status", "500");
+        jsonData.put("error", "Internal Server Error");
+        jsonData.put("path", request.getDescription(false));
+        jsonData.put("stack_trace", ExceptionUtils.getStackTrace(ex));
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(jsonData);
     }
 }
