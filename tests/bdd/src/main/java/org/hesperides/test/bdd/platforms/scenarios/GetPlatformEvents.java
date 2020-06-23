9f91094bae911e7b01fbe65766db0bb67e8c9507
Thomas L'Hostis <thomas.lhostis@gmail.com>
2020-06-23 18:02:18 +0200

    wip

--- tests/bdd/src/main/java/org/hesperides/test/bdd/platforms/scenarios/GetPlatformEvents.java
+++ tests/bdd/src/main/java/org/hesperides/test/bdd/platforms/scenarios/GetPlatformEvents.java
@@ -10,8 +10,10 @@ import org.hesperides.test.bdd.platforms.builders.PlatformBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 
-import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -31,34 +33,45 @@ public class GetPlatformEvents extends HesperidesScenario implements En {
         });
 
         Then("^the platform event at index (\\d) contains \"([^\"]*)\"(?: with old version \"([^\"]*)\" and new version \"([^\"]*)\")?$", (
-                Integer eventIndex, String changeName, String oldVersion, String newVersion) -> {
-            List<PlatformEventOutput> platformEvents = testContext.getResponseBodyAsList();
-            List<PlatformChangeOutput> changes = platformEvents.get(eventIndex).getChanges();
-            List<String> changesName = changes.stream().map(PlatformChangeOutput::getChangeName).collect(toList());
-            assertTrue(changesName.contains(changeName));
+                Integer eventIndex, String expectedChangeName, String oldVersion, String newVersion) -> {
+            List<PlatformEventOutput> actualPlatformEvents = testContext.getResponseBodyAsList();
+            List<PlatformChangeOutput> actualEventChanges = actualPlatformEvents.get(eventIndex).getChanges();
+            Map<String, PlatformChangeOutput> actualChangesByName = actualEventChanges.stream()
+                    .collect(toMap(PlatformChangeOutput::getChangeName, Function.identity()));
 
-            for (PlatformChangeOutput change : changes) {
-                if (change instanceof PlatformVersionUpdatedOutput && "platform_version_updated".equals(changeName)) {
-                    PlatformVersionUpdatedOutput platformVersionUpdated = (PlatformVersionUpdatedOutput) change;
-                    assertEquals(oldVersion, platformVersionUpdated.getOldVersion());
-                    assertEquals(newVersion, platformVersionUpdated.getNewVersion());
+            assertTrue(actualChangesByName.containsKey(expectedChangeName));
+            PlatformChangeOutput actualChange = actualChangesByName.get(expectedChangeName);
 
-                } else if (change instanceof DeployedModuleUpdatedOutput && "deployed_module_updated".equals(changeName)) {
-                    DeployedModuleUpdatedOutput deployedModuleVersionUpdated = (DeployedModuleUpdatedOutput) change;
-                    String currentVersion = deployedModuleBuilder.getVersion();
-                    deployedModuleBuilder.withVersion(oldVersion).buildPropertiesPath();
-                    assertEquals(deployedModuleBuilder.withVersion(oldVersion).buildPropertiesPath(), deployedModuleVersionUpdated.getOldPropertiesPath());
-                    assertEquals(deployedModuleBuilder.withVersion(newVersion).buildPropertiesPath(), deployedModuleVersionUpdated.getNewPropertiesPath());
-                    deployedModuleBuilder.withVersion(currentVersion).buildPropertiesPath();
+            if ("platform_created".equals(expectedChangeName)) {
+                assertTrue(actualChange instanceof PlatformCreatedOutput);
 
-                } else if (change instanceof DeployedModuleAddedOutput && "deployed_module_added".equals(changeName)) {
-                    DeployedModuleAddedOutput deployedModuleAdded = (DeployedModuleAddedOutput) change;
-                    assertEquals(deployedModuleBuilder.buildPropertiesPath(), deployedModuleAdded.getPropertiesPath());
+            } else if ("platform_version_updated".equals(expectedChangeName)) {
+                assertTrue(actualChange instanceof PlatformVersionUpdatedOutput);
+                PlatformVersionUpdatedOutput platformVersionUpdated = (PlatformVersionUpdatedOutput) actualChange;
+                assertEquals(oldVersion, platformVersionUpdated.getOldVersion());
+                assertEquals(newVersion, platformVersionUpdated.getNewVersion());
 
-                } else if (change instanceof DeployedModuleRemovedOutput && "deployed_module_removed".equals(changeName)) {
-                    DeployedModuleRemovedOutput deployedModuleRemoved = (DeployedModuleRemovedOutput) change;
-                    assertEquals(deployedModuleBuilder.buildPropertiesPath(), deployedModuleRemoved.getPropertiesPath());
-                }
+            } else if ("deployed_module_updated".equals(expectedChangeName)) {
+                assertTrue(actualChange instanceof DeployedModuleUpdatedOutput);
+                DeployedModuleUpdatedOutput deployedModuleVersionUpdated = (DeployedModuleUpdatedOutput) actualChange;
+                String currentVersion = deployedModuleBuilder.getVersion();
+                deployedModuleBuilder.withVersion(oldVersion).buildPropertiesPath();
+                assertEquals(deployedModuleBuilder.withVersion(oldVersion).buildPropertiesPath(), deployedModuleVersionUpdated.getOldPropertiesPath());
+                assertEquals(deployedModuleBuilder.withVersion(newVersion).buildPropertiesPath(), deployedModuleVersionUpdated.getNewPropertiesPath());
+                deployedModuleBuilder.withVersion(currentVersion).buildPropertiesPath();
+
+            } else if ("deployed_module_added".equals(expectedChangeName)) {
+                assertTrue(actualChange instanceof DeployedModuleAddedOutput);
+                DeployedModuleAddedOutput deployedModuleAdded = (DeployedModuleAddedOutput) actualChange;
+                assertEquals(deployedModuleBuilder.buildPropertiesPath(), deployedModuleAdded.getPropertiesPath());
+
+            } else if ("deployed_module_removed".equals(expectedChangeName)) {
+                assertTrue(actualChange instanceof DeployedModuleRemovedOutput);
+                DeployedModuleRemovedOutput deployedModuleRemoved = (DeployedModuleRemovedOutput) actualChange;
+                assertEquals(deployedModuleBuilder.buildPropertiesPath(), deployedModuleRemoved.getPropertiesPath());
+
+            } else {
+                throw new IllegalArgumentException("Event at index " + eventIndex + " does not contain change \"" + expectedChangeName + "\"");
             }
         });
     }
